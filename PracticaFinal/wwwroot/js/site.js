ede67b521e0970f2ac0f3ce9c131f61d716ae895
Anthony Reyes Duran <Anthony_Reyes_Duran@hotmail.com>
2021-10-19 00:48:25 -0400

    Versión Final

--- PracticaFinal/wwwroot/js/site.js
+++ PracticaFinal/wwwroot/js/site.js
@@ -1,110 +1,123 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Para dar formato de dinero.
+let MoneyFormat = new Intl.NumberFormat('en-US', {
 
-// Write your JavaScript code.
-var MoneyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
+
 });
 
-//A la hora de seleccionar el dropdown list reflejarse en el texto de lbAmount.
-//Automatico.
+//Ejecución Mostrar el monto disponible de la cuenta seleccionada.
 $(document).ready(function () {
+
     $('#dropdown1').on('change', function () {
-        document.getElementById("lbAmount").innerHTML = "RD" + MoneyFormat.format(this.value);
+
+        const amount = document.getElementById('lbAmount');
+        amount.textContent = 'RD' + MoneyFormat.format(this.value);
+
     })
+
 });
 
-//Manual.
-function getComboFromData(selectObject) {
-    var value = selectObject.value;
-    document.getElementById("lbAmount").innerHTML = "RD" + MoneyFormat.format(value);
+//Función para evitar transferir al misma cuenta. (Validaciones)
+function fieldValidations() {
+
+    const data1 = document.getElementById('dropdown1');
+    const data2 = document.getElementById('dropdown2');
+    const amount = parseFloat(document.getElementById('tbMonto').value);
+    const account1 = parseInt(data1.options[data1.selectedIndex].text);
+    const account2 = parseInt(data2.options[data2.selectedIndex].text);
+    const amAccount1 = parseFloat(document.getElementById('dropdown1').value);
+    const btnAccess = document.getElementById("btnsubmit");
+
+    if (account1 !== account2 && amount < amAccount1 && amount !== 0) {
+
+        btnAccess.disabled = false;
+
+    }
+    else {
+
+        btnAccess.disabled = true;
+
+    }
+
 }
 
-//Función para evitar transferir al misma cuenta.
+//Ejecución para traer el botón en modo deshabilitado como predeterminado.
 $(document).ready(function () {
-    $('#dropdown1').on('change', function () {
-        if (document.getElementById("dropdown1").value == document.getElementById("dropdown2").value
-            || document.getElementById("tbMonto").value == ""
-            || document.getElementById("dropdown1").value < document.getElementById("tbMonto").value){
-            document.getElementById("btnsubmit").disabled = true;
-        }
-        else {
-            document.getElementById("btnsubmit").disabled = false;
-        }
-    })
 
-    $('#dropdown2').on('change', function () {
-        if (document.getElementById("dropdown1").value == document.getElementById("dropdown2").value
-            || document.getElementById("tbMonto").value == ""
-            || document.getElementById("dropdown1").value < document.getElementById("tbMonto").value){
-            document.getElementById("btnsubmit").disabled = true;
-        }
-        else {
-            document.getElementById("btnsubmit").disabled = false;
-        }
-    })
+    $('#btnsubmit').attr('disabled', true);
 
-    $('#tbMonto').on('change', function () {
-        if (document.getElementById("dropdown1").value == document.getElementById("dropdown2").value
-            || document.getElementById("tbMonto").value == ""
-            || document.getElementById("dropdown1").value < document.getElementById("tbMonto").value){
-            document.getElementById("btnsubmit").disabled = true;
-        }
-        else {
-            document.getElementById("btnsubmit").disabled = false;
-        }
-    })
-});
-
-//deshabilitar boton de transferir.
-$(document).ready(function () {
-    $('#btnsubmit').attr('disabled',true);
 })
 
-//creando una variable de para el botón Cancel.
+//Creando una variable de para el botón Cancel. (lógica interna).
 $(document).ready(function () {
+
     $('#btncancel').on('click', function () {
-        document.getElementById("goBack").value = "y";
+
+        const goBack = document.getElementById("goBack");
+        goBack.value = "y";
+
     })
+
 })
 
-//asignar variable para obtener el AccountNumber de forma autamática.
-function getAccountNumber(object) {
-    document.getElementById("inAccountNumber").value = object;
+//Asignar variable para obtener el AccountNumber de forma automática.
+function getAccountNumber(number) {
+
+    const accNumber = document.getElementById("inAccountNumber");
+    accNumber.value = number;
+
 }
 
-//Aplicar accion para filtrar por fecha.
-//date1
+//Ejecución para filtrar por fecha.
+//Campo de fecha 1.
 $(document).ready(function () {
-    $('#date1').on('change', function () {
-        var d1 = document.getElementById("date1").value;
-        var d2 = document.getElementById("date2").value;
 
-        if (d2 < d1) {
-            document.getElementById("date1").value = document.getElementById("date2").value;
-            document.getElementById("dateError").innerHTML = "Fecha invalida";
+    $('#date1').on('change', function () {
+
+        const data1 = document.getElementById('date1');
+        const data2 = document.getElementById('date2');
+        const dateError = document.getElementById('dateError');
+        const dateForm = document.getElementById("dateForm");
+
+        if (data2.value < data1.value) {
+
+            data1.value = data2.value;
+            dateError.textContent = 'Fecha invalida';
+
         }
         else {
-            document.getElementById("dateForm").submit();
-        }
 
+            dateForm.submit();
+
+        }
        
     })
+
 })
 
-//date2
+//Campo de fecha 2.
 $(document).ready(function () {
-    $('#date2').on('change', function () {
-        var d1 = document.getElementById("date1").value;
-        var d2 = document.getElementById("date2").value;
 
-        if (d2 < d1) {
-            document.getElementById("date1").value = document.getElementById("date2").value;
-            document.getElementById("dateError").innerHTML = "Fecha invalida";
+    $('#date2').on('change', function () {
+
+        const data1 = document.getElementById('date1');
+        const data2 = document.getElementById('date2');
+        const dateError = document.getElementById('dateError');
+        const dateForm = document.getElementById("dateForm");
+
+        if (data2.value < data1.value) {
+
+            data2.value = data1.value;
+            dateError.textContent = 'Fecha invalida';
+
         }
         else {
-            document.getElementById("dateForm").submit();
+
+            dateForm.submit();
+
         }
+
     })
+
 })
